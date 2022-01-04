edde99f3e798521e927dc8126467eeb2bd9f11ce
ildaserj <serjanaj.ilda@gmail.com>
2022-01-04 16:16:45 +0100

    ResourceFileTest.cpp adding

--- Doctest/ResourceFileTest.cpp
+++ Doctest/ResourceFileTest.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "doctest.h"
+
+#include "../ResourceFile.h"
+
+TEST_CASE("ResourceFile test Costructor"){
+ResourceFile *r[2];
+r[0] = new ResourceFile("Risorsa_01", 4);
+CHECK("Risorsa_01" == r[0]->getFileName());
+CHECK(4 ==  r[0]->getBit());
+r[1] = new ResourceFile("Risorsa_02", 8);
+CHECK("Risorsa_02" == r[1]->getFileName());
+CHECK(8 == r[1]->getBit());
+r[2] = new ResourceFile("Risorsa_03", 16);
+CHECK("Risorsa_03" == r[1]->getFileName());
+CHECK(16 == r[2]->getBit());
+}
+
